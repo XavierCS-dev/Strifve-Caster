84537457cb5f35a47f6f5d0cfd09c2b9dc5e058a
XavierCyber <xavierelwell@gmail.com>
2023-05-09 18:08:03 +0100

    init

---
+++ src/engine/render_data.rs
@@ -0,0 +1,109 @@
+use winit::window::Window;
+
+pub struct RenderData {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    surface: wgpu::Surface,
+    window: Window,
+}
+
+impl RenderData {
+    pub async fn new(window: Window) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: wgpu::Dx12Compiler::default(),
+        });
+
+        let surface = unsafe { instance.create_surface(&window).unwrap() };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: adapter.features(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let format = surface_capabilities.formats[0];
+        let size = window.inner_size();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::AutoVsync,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: Vec::new(),
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            device,
+            queue,
+            config,
+            size,
+            surface,
+            window,
+        }
+    }
+
+    pub fn render(&self) -> Result<(), wgpu::SurfaceError> {
+        // FROM LEARWGPU, PLACEHOLDER FOR TESTING, REMOVE LATER
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn resize(&self) -> Result<(), String> {
+        todo!()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+mod engine;
+use engine::render_data::{self, RenderData};
+
+pub async fn run() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Learn WGPU")
+        .build(&event_loop)
+        .unwrap();
+    window.set_resizable(false);
+    let render_data = RenderData::new(window).await;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::CloseRequested,
+            } if window_id == render_data.window().id() => *control_flow = ControlFlow::Exit,
+            Event::RedrawRequested(window_id) if window_id == render_data.window().id() => {
+                render_data.render().unwrap();
+            }
+            Event::RedrawEventsCleared => {
+                render_data.window().request_redraw();
+            }
+            _ => (),
+        }
+    })
+}
