434ab527f53f48e4b5b04e692726b26f71939966
XavierCyber <xavierelwell@gmail.com>
2023-06-26 13:01:58 +0100

    Clean RenderData

--- src/engine/render_data.rs
+++ src/engine/render_data.rs
@@ -28,32 +28,6 @@ pub struct RenderData {
     walls: HashMap<u32, Vec<Entity2D>>,
 }
 
-// Temp, to be removed
-const VERTICES: &[Vertex2D] = &[
-    Vertex2D {
-        position: [-0.0868241, 0.49240386],
-        tex_pos: [1.0 - 0.4131759, 1.0 - 0.99240386],
-    }, // A
-    Vertex2D {
-        position: [-0.49513406, 0.06958647],
-        tex_pos: [1.0 - 0.0048659444, 1.0 - 0.56958647],
-    }, // B
-    Vertex2D {
-        position: [-0.21918549, -0.44939706],
-        tex_pos: [1.0 - 0.28081453, 1.0 - 0.05060294],
-    }, // C
-    Vertex2D {
-        position: [0.35966998, -0.3473291],
-        tex_pos: [1.0 - 0.85967, 1.0 - 0.1526709],
-    }, // D
-    Vertex2D {
-        position: [0.44147372, 0.2347359],
-        tex_pos: [1.0 - 0.9414737, 1.0 - 0.7347359],
-    }, // E
-];
-
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /**/ 2, 3, 4];
-
 impl RenderData {
     pub async fn new(window: Window) -> Self {
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
@@ -133,7 +107,9 @@ impl RenderData {
         let mut walls: HashMap<u32, Vec<Entity2D>> = HashMap::new();
         walls.insert(entity_one.texture_id(), vec![entity_one]);
         walls.insert(entity_two.texture_id(), vec![entity_two]);
-        let wall_batches: Vec<Batch2D> = Vec::new();
+        let mut wall_batches: Vec<Batch2D> = Vec::new();
+        wall_batches.push(batch_one);
+        wall_batches.push(batch_two);
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("shader"),
@@ -193,22 +169,6 @@ impl RenderData {
             }),
             multiview: None,
         });
-
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: bytemuck::cast_slice(VERTICES),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-
-        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Index Buffer"),
-            contents: bytemuck::cast_slice(INDICES),
-            usage: wgpu::BufferUsages::INDEX,
-        });
-
-        // temp, vertices_count will be init with 0 when this is gone.
-        let index_count: u32 = INDICES.len() as u32;
-
         Self {
             device,
             queue,
@@ -217,6 +177,7 @@ impl RenderData {
             surface,
             window,
             pipeline,
+            wall_batches,
             walls,
         }
     }
